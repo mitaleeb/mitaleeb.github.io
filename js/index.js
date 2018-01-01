23772d15d075c989d031852e228b594b78e9ad19
mitaleeb <mitalee.bharadwaj@gmail.com>
2017-12-31 22:53:15 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -39,7 +39,7 @@ function initializeClock(id, endtime) {
 
 
 var now = new Date();
-var deadline = new Date(Date.parse(new Date('1/1/2018 12:00 AM')));
+var deadline = new Date(Date.parse(new Date('1/15/2018 12:00 AM')));
 var difference = deadline - now;
 
 initializeClock('clockdiv', deadline);
